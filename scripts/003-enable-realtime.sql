d40d22f975c0f1b64130c1b553bf0cca053f5ceb
Nicebott <151235738+Nicebott@users.noreply.github.com>
2026-02-19 19:38:50 -0400

    Delete scripts directory

--- scripts/003-enable-realtime.sql
+++
@@ -1,3 +0,0 @@
--- Enable realtime for chat_messages and forum_messages tables
-ALTER PUBLICATION supabase_realtime ADD TABLE chat_messages;
-ALTER PUBLICATION supabase_realtime ADD TABLE forum_messages;
